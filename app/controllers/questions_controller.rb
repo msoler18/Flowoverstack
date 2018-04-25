7368fab3901066df87b86bcfc33cf1c10be133f9
msoler18 <msoler735@gmail.com>
2018-04-24 19:28:55 -0500

    Add functionality for create questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,25 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path, notice: "Question has been created"
+    else
+      render :new
+    end  
+  end
+
+
+
+
+  private
+  def question_params
+    params.require(:question).permit(:title,:content)
+  end
 end
