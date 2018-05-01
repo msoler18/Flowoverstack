592d66117e7446a4bb4f7320aa7e82fdb2b9d743
msoler18 <msoler735@gmail.com>
2018-04-30 22:02:05 -0500

    Add scripts and add relations between answers and questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425001219) do
+ActiveRecord::Schema.define(version: 20180501025414) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
