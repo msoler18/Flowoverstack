3400f6977fb7173c51e66c91c9b7333f30480669
msoler18 <msoler735@gmail.com>
2018-04-30 22:08:20 -0500

    Add nested Routes for Questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :answers
-  resources :questions
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "questions#index"
+  resources :questions do
+    resources :answers   
+  end  
 end
