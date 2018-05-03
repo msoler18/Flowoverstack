a847c8f4454d5993b9c21a84eeb8baf1a80b9f74
msoler18 <msoler735@gmail.com>
2018-05-02 20:14:09 -0500

    Add Create answer Function

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,27 @@
 class AnswersController < ApplicationController
+  
+  def index
+    @answers = Answer.all
+  end 
+
   def new
     @question = Question.find(params[:question_id])
     @answer = @question.answers.new
   end
-    
+  
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.create(answer_params)
+    if @answer.save
+      redirect_to questions_answer_path
+    else
+      render :new  
+    end
+  end  
 
+
+  private
+  def answer_params
+    params.require(:answer).permit(:content)
+  end
 end
